7f194c0c31d22c0642e80c2bae586c0de1afed13
Jonathan Perron <jonathan.perron@lumapps.com>
2024-05-09 16:05:26 +0200

    feat(exception): add handling of navitia error messages

--- tests/client/apis/test_api_base_client.py
+++ tests/client/apis/test_api_base_client.py
@@ -1,4 +1,12 @@
+import pytest
+from requests import Response  # type: ignore
 from navitia_client.client.apis.api_base_client import ApiBaseClient
+from navitia_client.client.exceptions import (
+    NavitiaAccessTokenMissingError,
+    NavitiaForbiddenAccessError,
+    NavitiaUnableToParseError,
+    NavitiaUnknownObjectError,
+)
 
 
 def test_http_base_client() -> None:
@@ -12,3 +20,46 @@ def test_http_base_client() -> None:
 
     # Then
     assert isinstance(client, ApiBaseClient)
+
+
+@pytest.fixture
+def api_base_client():
+    return ApiBaseClient(
+        auth_token="foobar", base_navitia_url="https://api.navitia.io/v1/"
+    )
+
+
+def test_check_response_for_exception(api_base_client: ApiBaseClient) -> None:
+    # Given
+    response = Response()
+    response._content = (
+        b'{"error": {"id": "unknown_object", "message": "Unable to find place: 0;0"}}'
+    )
+    # When/Then
+    with pytest.raises(NavitiaUnknownObjectError):
+        api_base_client._check_response_for_exception(response)
+
+    # Given
+    response = Response()
+    response._content = (
+        b'{"error": {"id": "unable_to_parse", "message": "Unable to parse : {0;0}"}}'
+    )
+    # When/Then
+    with pytest.raises(NavitiaUnableToParseError):
+        api_base_client._check_response_for_exception(response)
+
+    # Given
+    response = Response()
+    response._content = b'{"message": "no token"}'
+    # When/Then
+    with pytest.raises(NavitiaAccessTokenMissingError):
+        api_base_client._check_response_for_exception(response)
+
+    # Given
+    response = Response()
+    response._content = (
+        b'{"message": "It is either read-protected or not readable by the server."}'
+    )
+    # When/Then
+    with pytest.raises(NavitiaForbiddenAccessError):
+        api_base_client._check_response_for_exception(response)
