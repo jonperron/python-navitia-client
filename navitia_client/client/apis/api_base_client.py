4d2304613ad36468b1e693abac1d844dfaf286fa
Jonathan Perron <jonathan@perron.bzh>
2024-05-09 19:21:30 +0200

    feat(exception): add handling of navitia error messages (#29)

--- navitia_client/client/apis/api_base_client.py
+++ navitia_client/client/apis/api_base_client.py
@@ -1,5 +1,12 @@
 from requests import Response, Session  # type: ignore
 
+from navitia_client.client.exceptions import (
+    NavitiaAccessTokenMissingError,
+    NavitiaForbiddenAccessError,
+    NavitiaUnknownObjectError,
+    NavitiaUnableToParseError,
+)
+
 
 class ApiBaseClient:
     def __init__(self, auth_token: str, base_navitia_url: str) -> None:
@@ -7,5 +14,25 @@ class ApiBaseClient:
         self.session = Session()
         self.session.headers.update({"Authorization": auth_token})
 
+    @staticmethod
+    def _check_response_for_exception(response: Response) -> Response:
+        json_payload = response.json()
+        if "error" in json_payload:
+            error_message = json_payload["error"]["message"]
+            match json_payload["error"]["id"]:
+                case "unable_to_parse":
+                    raise NavitiaUnableToParseError(error_message)
+                case "unknown_object":
+                    raise NavitiaUnknownObjectError(error_message)
+
+        if "message" in json_payload:
+            error_message = json_payload["message"]
+            if "no token" in error_message:
+                raise NavitiaAccessTokenMissingError(error_message)
+            if " either read-protected or not readable" in error_message:
+                raise NavitiaForbiddenAccessError(error_message)
+
+        return response
+
     def get_navitia_api(self, endpoint: str) -> Response:
-        return self.session.get(endpoint)
+        return self._check_response_for_exception(self.session.get(endpoint))
---
+++ navitia_client/client/exceptions.py
@@ -0,0 +1,14 @@
+class NavitiaAccessTokenMissingError(Exception):
+    pass
+
+
+class NavitiaForbiddenAccessError(Exception):
+    pass
+
+
+class NavitiaUnknownObjectError(Exception):
+    pass
+
+
+class NavitiaUnableToParseError(Exception):
+    pass
